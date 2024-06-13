f276afc95af2344e8427fbea41289ac0dcc26a4f
Andrea Sgura <andre-56@hotmail.it>
2024-06-13 21:29:56 +0200

    con qualche bug

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,15 +1,39 @@
 <template>
     <div>
-        <h1>Home</h1>
+        <h1>Proiezioni della settimana</h1>
+
+        <ul>
+            <li v-for="projection in projections" :key="projection.id">
+                {{ projection.movie.title }}, data proiezione: {{ projection.date_projection }}
+            </li>
+        </ul>
+
     </div>
 </template>
 
 <script>
-    export default {
-        name: "AppHome",
+import axios from "axios";
+import { store } from "../store";
+export default {
+    name: "AppHome",
+    data() {
+        return {
+            store,
+            projections: []
+        }
+    },
+    methods: {
+        getWeeklyProjections() {
+            axios.get(this.store.apiBaseUrl + "/weekly_movies").then((response) => {
+                console.log(response.data);
+                this.projections = response.data.results;
+            })
+        }
+    },
+    mounted() {
+        this.getWeeklyProjections();
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
