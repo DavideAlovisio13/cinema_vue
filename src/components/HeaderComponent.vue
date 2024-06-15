9a11806ac681d724767e273de1d95e2dc33e9558
Nik0-9 <filannino.nicola9@gmail.com>
2024-06-15 17:12:55 +0200

    area admin spostato a destra

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,30 +3,19 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li
-              class="nav-item"
-              v-for="(item, index) in menuItems"
-              :key="index"
-            >
+          <ul class="navbar-nav d-flex w-100">
+            <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
               <router-link class="nav-link" :to="{ name: item.routeName }">
                 {{ item.name }}
               </router-link>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="http://localhost:8000">Admin Area</a>
+            <li class="nav-item ms-auto">
+              <a class="nav-link" href="http://localhost:8000">Admin Area</a>
             </li>
           </ul>
         </div>
