f194ff6dad8fb31a7899fa07182a689922bbdf8e
DavideAlovisio13 <alovisiod@gmail.com>
2024-06-15 18:29:12 +0200

    do some styles changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <HeaderComponent />
+  <JumboComponent />
   <main>
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="MoveUp">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </main>
 </template>
 
 <script>
 import axios from "axios";
 import { store } from "./store";
-
+import JumboComponent from "./components/JumboComponent.vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
 export default {
   name: "App",
   components: {
     HeaderComponent,
+    JumboComponent
   },
   data() {
     return {
@@ -31,12 +37,53 @@ export default {
         this.$router.push({ name: 'not-found' });
       }).finally();
     },
+    getRooms() {
+      axios.get(this.store.apiBaseUrl + "/rooms").then((response) => {
+        console.log(response.data);
+        this.store.rooms = response.data.results;
+      });
+    },
   },
 
   mounted() {
     this.getMovies();
+    this.getRooms();
+    console.log(this.store.rooms)
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// transitions css
+.moveUp-enter-active {
+  animation: fadeIn 3s ease-in
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 50;
+  }
+
+  100% {
+    opacity: 100;
+  }
+}
+
+.moveUp-leave-active {
+  animation: moveUp 3s ease-in;
+}
+
+@keyframes moveUp {
+  0% {
+    transform: translateY(0);
+  }
+
+  100% {
+    transform: translateY(-400px);
+  }
+}
+</style>
