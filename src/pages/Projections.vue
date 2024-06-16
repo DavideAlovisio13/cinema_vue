747f7756261dbf58f65ccf8236fab3f4b51d3a6c
AngeloC89 <Angelociulla89@gmail.com>
2024-06-16 23:29:45 +0200

    changed projection page style but not finished

--- src/pages/Projections.vue
+++ src/pages/Projections.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="main-container">
-        <h1>Projections</h1>
-        <ul>
-            <li v-for="item in projections" :key="item.id">
+    <h1 class="container">Projections</h1>
+    <div class="container first">
+       
+        <div class="container proj" v-for="item in projections" :key="item.id">
+            <div class="left">
                 <span>
                     data: {{ item.date_projection }}
                 </span>
@@ -18,37 +19,86 @@
                 <span>&nbsp;
                     orario: {{ item.slot.time_slot }}
                 </span>
+            </div>
+            <div class="right">
+                <img class="img-fluid" :src=" item.movie.thumb" alt="">
 
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import axios from "axios";
-import { store } from "../store";
-export default {
-    name: "Projections",
+    import axios from "axios";
+    import { store } from "../store";
+    export default {
+        name: "Projections",
 
-    data() {
-        return {
-            store,
-            projections: [],
-        };
-    },
-    methods: {
-        getProjections() {
-            axios.get(this.store.apiBaseUrl + "/projections").then((response) => {
-                console.log(response.data);
-                this.projections = response.data.results;
-
-            });
+        data() {
+            return {
+                store,
+                projections: [],
+            };
         },
-    },
-    mounted() {
-        this.getProjections();
-    },
-};
+        methods: {
+            getProjections() {
+                axios.get(this.store.apiBaseUrl + "/projections").then((response) => {
+                    console.log(response.data);
+                    this.projections = response.data.results;
+
+                });
+            },
+        },
+        mounted() {
+            this.getProjections();
+        },
+    };
 </script>
 
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.first{
+    height: 500px;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+}
+
+.proj{
+    width: 1000px;
+    height: 300px;
+    border: 1px solid black;
+    border-radius: 20px;
+    margin: 30px auto;
+   display: flex;
+   
+
+    .left{
+        width: 50%;
+        height: 100%;
+        border-right: 1px solid black;
+        font-size: 2rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        
+        
+        
+    }
+    .right{
+        width: 50%;
+        height: 100%;
+        overflow: hidden;
+
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+    }
+    
+}
+
+
+</style>
