7e7b9333a781f64f9c01a9689fd267ccb3be0a6c
samuelescavello <scavellosamuele23@gmail.com>
2024-06-16 16:21:46 +0200

    estetica footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,119 +2,118 @@
     <footer>
         <div class="first">
             <img src="https://cdn.dribbble.com/users/1413861/screenshots/4562913/animated-c.gif" alt="placeholder">
-            <h2 class="text-center m-auto">Immergiti nell'universo cinematografico di Boolean.</h2>
+            <h2 class="text-center m-auto text-white" id="title">Immergiti nell'universo cinematografico di Boolean.</h2>
 
         </div>
         <div class="second ">
             <div class="left">
                 <div class="p-3 me-5">
-                    <h5>Contact</h5>
-                    <ul class="list-unstyled">
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
+                    <h5>SOCIAL</h5>
+                    <ul class="list-unstyled text-center">
+                        <li><a class="text-decoration-none" href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i> instagram</a></li>
+                        <li><a class="text-decoration-none" href="https://www.facebook.com/?locale=it_IT"><i class="fa-brands fa-facebook"></i> facebook</a></li>
+                        <li><a class="text-decoration-none" href="https://x.com/i/flow/login"><i class="fa-brands fa-twitter"></i> twitter</a></li>
+                        <li><a class="text-decoration-none" href="https://www.tiktok.com/login"><i class="fa-brands fa-tiktok"></i> tiktok</a></li>
                     </ul>
                 </div>
                 <div class="p-3 mx-5">
-                    <h5>About</h5>
-                    <ul class="list-unstyled">
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
+                    <h5>NOTE LEGALI</h5>
+                    <ul class="list-unstyled text-center">
+                        <li><a class="text-decoration-none" href="">Privacy policy</a></li>
+                        <li><a class="text-decoration-none" href="">Cookie policy</a></li>
+                        <li><a class="text-decoration-none" href="">Condizioni di utilizzo</a></li>
+                        <li><a class="text-decoration-none" href="">Regolamenti</a></li>
+                        
                     </ul>
                 </div>
                 <div class="p-3 mx-5">
-                    <h5>Shop</h5>
-                    <ul class="list-unstyled">
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
-                        <li><a class="text-decoration-none" href="">Link</a></li>
+                    <h5>CINEX</h5>
+                    <ul class="list-unstyled text-center">
+                        <li><a class="text-decoration-none" href="">chi siamo</a></li>
+                        <li><a class="text-decoration-none" href="">i nostri cinema</a></li>
                     </ul>
                 </div>
             </div>
-            <div class="right">
-                <h5>Title</h5>
-                <p>indirizzi e dati di contatto</p>
+            <div class="right d-flex align-items-center justify-content-center">
+                <img src="/images/logo.png" alt="">
             </div>
         </div>
     </footer>
 </template>
 
 <script>
-    export default {
-        name: "FooterComponent"
-    }
+export default {
+    name: "FooterComponent"
+}
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/_variables.scss' as *;
 
-    footer {
-        height: 600px;
-   
+footer {
+    height: 600px;
 
-        .first {
-           color: gold;
-            height: 300px;
+
+    .first {
+        height: 300px;
+        display: flex;
+        background-color: $color-blue;
+        padding: 15px;
+    }
+
+
+    .second {
+
+        height: 300px;
+        display: flex;
+        background-color: #E8B757;
+        color: white;
+
+        .left {
+            width: 50%;
             display: flex;
-            background-color: rgba(33, 32, 32, 0.998);
+
+            h5 {
+                text-align: center;
+            }
+
+            ul {
+
+
+                li {
+                    padding: 5px 20px;
+                    i{
+                        font-size: 25px;
+                    }
+
+                    &:hover {
+                        background-color: rgba(188, 188, 188, 0.059);
+                        cursor: pointer;
+                    }
+
+                    &:hover a {
+                        color: white;
+
+                    }
+
+
+                    a {
+                        color: #E87E66;
+
+
+
+                    }
+                }
+            }
+
+        }
+
+        .right {
+            width: 50%;
             padding: 15px;
         }
 
 
-        .second {
-            
-            height: 300px;
-            display: flex;
-            background-color: rgb(1, 1, 43);
-            color: white;
-
-            .left {
-                width: 50%;
-                display: flex;
-                h5{
-                    text-align: center;
-                }
-
-                ul {
-
-
-                    li {
-                        padding: 5px 20px;
-
-                        &:hover {
-                            background-color: rgba(188, 188, 188, 0.059);
-                            cursor: pointer;
-                        }
-                        &:hover a {
-                            color: rgb(255, 0, 0);
-                        }
-
-                        a {
-                            color: rgb(255, 255, 255);
-
-
-
-                        }
-                    }
-                }
-
-            }
-
-            .right {
-                width: 50%;
-                padding: 15px;
-            }
-
-
-        }
     }
+}
 </style>
