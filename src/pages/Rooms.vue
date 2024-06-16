3746795dac55a3678fa1632d5adb0391efc93179
DavideAlovisio13 <alovisiod@gmail.com>
2024-06-16 17:02:14 +0200

    add some style changes

--- src/pages/Rooms.vue
+++ src/pages/Rooms.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="main-container">
-    <h1>Rooms</h1>
-    <ul>
-      <li v-for="room in rooms" :key="room.id">{{ room.name }}</li>
-    </ul>
+    <section class="showcase">
+      <div class="container-fluid p-0">
+        <div class="row g-0" v-for="room in rooms" :key="room.id">
+          <div class="col-lg-6 order-lg-2 text-white showcase-img"
+            :style="{ backgroundImage: `url(${room.img_room})`}"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+            <h2>Fully Responsive Design</h2>
+            <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look
+              great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 
 </template>
