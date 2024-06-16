3425071f3e2ed47268f6e09b24329ab2ea15a1d2
sashapoliti <sas.politi@gmail.com>
2024-06-17 01:55:07 +0200

    modal bg

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -1,28 +1,47 @@
 <template>
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  <button
+    type="button"
+    class="btn btn-primary"
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal"
+  >
     <div class="card">
       <img :src="movie.thumb" class="card-img-top" :alt="movie.title" />
     </div>
   </button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">...</div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -45,15 +64,15 @@ export default {
 
 <style lang="scss" scoped>
 button {
-    width: 100%;
-    margin: 0;
-    padding: 0;
-    border: none;
-    border-radius: 20px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  border: none;
+  border-radius: 20px;
   .card {
     height: 400px;
-    overflow: hidden; 
-    border-radius: 20px;  
+    overflow: hidden;
+    border-radius: 20px;
     border: 4px solid #da654b;
     cursor: pointer;
     &:hover {
@@ -65,6 +84,15 @@ button {
       height: 100%;
       object-fit: cover;
       object-position: top;
+    }
+  }
+}
+.modal-dialog {
+  max-width: 50%;
+  .modal-content {
+    overflow: hidden;
+    .modal-body {
+      padding: 0 40px;
     }
   }
 }
